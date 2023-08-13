6ed1ba22b4b1f08bc504618600c48e310a5e9ee4
OnlineAzamat <yakubbaev.azamat@mail.ru>
2023-08-13 20:04:23 +0500

    arr. spriteAnimations updates

--- script.js
+++ script.js
@@ -26,12 +26,14 @@ animationStates.forEach((state, index) => {
   let frames = {
     loc: [],
   }
-  for (let j = 0; j < state.frames; s++) {
+  for (let j = 0; j < state.frames; j++) {
     let positionX = j * spriteWidth;
     let positionY = index * spriteHeight;
     frames.loc.push({ x: positionX, y: positionY});
   }
+  spriteAnimations[state.name] = frames;
 })
+console.log(animationStates);
 
 function animate(){
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -44,4 +46,4 @@ function animate(){
   requestAnimationFrame(animate);
 }
 animate();
-// 23:10
+// 36:03
